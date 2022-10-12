01acda1fc24807a30ca72465b48e0f420ec7c81a
seoa <tjdkzz97@naver.com>
2022-10-12 18:49:53 +0900

    project create and add css

--- src/App.js
+++ src/App.js
@@ -3,22 +3,69 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    {/* 1011 -> 반응형 css 정리하기 */}
+
+
+    {/* Nav */}
+    <div className='nav'>
+      {/* 제목 */}
+      <div className='nav-title'>
+        <p>Seoa's film</p>
+      </div >
+      {/* 메뉴 */}
+      <div className='nav-menu'>
+        <div className='nav-menus'>목록</div>
+        <div className='nav-menus'>상세</div>
+        <div className='nav-menus'>리스트</div>
+      </div>
     </div>
+
+    <div className='main-block'>
+      <div className='main-video'>
+        
+      </div>
+    </div>
+    
+
+    <div className='main-block'>
+      <div className='main-content01'>
+        {/* 카테고리 */}
+        <div className='main-content01-cate'>
+          <p>카테고리</p>
+          <p>카테고리</p>
+          <p>카테고리</p>
+          <p>카테고리</p>
+          <p>카테고리</p>
+        </div>
+
+        {/* 사진 */}
+        <div className='main-content01-pic'>
+
+          {/* web:3 / mobile:2 */}
+          <div className='main-pictures'></div>
+          <div className='main-pictures'></div>
+          <div className='main-pictures'></div>
+          <div className='main-pictures'></div>
+          <div className='main-pictures'></div>
+          <div className='main-pictures'></div>
+        </div>
+
+        {/* 더보기 */}
+        <div>
+          <div>더보기</div>
+        </div>
+      </div>
+    </div>
+
+    <div className='main-block'>
+      <div className='main-content01'>
+
+      </div>
+    </div>
+
+
+    </>
   );
 }
 
