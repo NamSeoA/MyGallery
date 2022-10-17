422e09c9d5cf92481d1dafeecfce6df2ab884ba6
seoa <tjdkzz97@naver.com>
2022-10-17 22:49:41 +0900

    change main page layout

--- src/App.js
+++ src/App.js
@@ -4,31 +4,42 @@ import './App.css';
 function App() {
   return (
     <>
-    {/* 1011 -> 반응형 css 정리하기 */}
-
 
     {/* Nav */}
     <div className='nav'>
+      <div className='nav-center'>
       {/* 제목 */}
       <div className='nav-title'>
-        <p>Seoa's film</p>
+        <img src={require('./img/logo.png')} />
       </div >
       {/* 메뉴 */}
-      <div className='nav-menu'>
-        <div className='nav-menus'>목록</div>
-        <div className='nav-menus'>상세</div>
-        <div className='nav-menus'>리스트</div>
+      <ul className='nav-menu'>
+        <li className='nav-menus'>ABOUT</li>
+        <li className='nav-menus'>LIST</li>
+        <li className='nav-menus'>SEEN</li>
+      </ul>
       </div>
     </div>
-
-    <div className='main-block'>
+    
+    <div className='main-block-top'>
+      <div className='main-title'>
+        <p className='main-title-1'>UNFORGETTABLE.</p>
+        <p className='main-title-2'>Moments that pass by casually in a busy life, joyous and beautiful times, or inspired moments.<br/>
+        I don't want that moments to be forgotten. That's why I left them in the film.</p>
+      </div>
       <div className='main-video'>
         
+      </div>
+      <div className='main-line'>
+        <hr className='main-line-hr' />
       </div>
     </div>
     
 
     <div className='main-block'>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 47 1">
+        <path d="M13.38,33.31h47" transform='translate(-13.38 -32.81)'></path>
+      </svg>
       <div className='main-content01'>
         {/* 카테고리 */}
         <div className='main-content01-cate'>
